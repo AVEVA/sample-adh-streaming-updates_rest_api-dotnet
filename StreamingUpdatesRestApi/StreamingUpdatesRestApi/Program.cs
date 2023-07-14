3e3fb1bf5c0174ba65715ed2d14f6c9e97716588
Kien In <kien.in@aveva.com>
2023-07-14 11:06:27 -0700

    add appsettings for adh-alpha testing

--- StreamingUpdatesRestApi/StreamingUpdatesRestApi/Program.cs
+++ StreamingUpdatesRestApi/StreamingUpdatesRestApi/Program.cs
@@ -10,13 +10,18 @@ namespace StreamingUpdatesRestApi
 
         public static void Main()
         {
+            MainAsync().GetAwaiter().GetResult();
+        }
 
+        public static async Task<bool> MainAsync()
+        {
+            // Make a copy of appsettings.placeholder.json and replace credentials prior to running
             _configuration = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)
                     .AddJsonFile("appsettings.json")
                     .AddJsonFile("appsettings.test.json", optional: true)
                     .Build();
-
+            
             string tenantId = _configuration["TenantId"];
             string namespaceId = _configuration["NamespaceId"];
             string resource = _configuration["Resource"];
@@ -24,8 +29,17 @@ namespace StreamingUpdatesRestApi
             string clientSecret = _configuration["ClientSecret"];
 
             Uri uriResource = new(resource);
+
+            // Step 1
+            // Obtain authentication handler for ADH using Client-credential clients
             AuthenticationHandler authenticationHandler = new(uriResource, clientId, clientSecret);
             SdsService sdsService = new(new Uri(resource), authenticationHandler);
+            ISdsMetadataService metadataService = sdsService.GetMetadataService(tenantId, namespaceId);
+            var stream = await metadataService.GetStreamAsync("KangeeTest");
+
+            Console.WriteLine(stream);
+
+            return true;
         }
     }
 }
