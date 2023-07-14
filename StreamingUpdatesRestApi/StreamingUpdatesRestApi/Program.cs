01c8b5ba8de02cf22541c7d59f1686124398b3e0
Kien In <kien.in@aveva.com>
2023-07-14 09:47:14 -0700

    add project references

--- StreamingUpdatesRestApi/StreamingUpdatesRestApi/Program.cs
+++ StreamingUpdatesRestApi/StreamingUpdatesRestApi/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using OSIsoft.Data;
+using OSIsoft.Identity;
+
+namespace StreamingUpdatesRestApi
+{
+    public static class Program
+    {
+        private static IConfiguration _configuration;
+
+        public static void Main()
+        {
+
+            _configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile("appsettings.test.json", optional: true)
+                    .Build();
+
+            string tenantId = _configuration["TenantId"];
+            string namespaceId = _configuration["NamespaceId"];
+            string resource = _configuration["Resource"];
+            string clientId = _configuration["ClientId"];
+            string clientSecret = _configuration["ClientSecret"];
+
+            Uri uriResource = new(resource);
+            AuthenticationHandler authenticationHandler = new(uriResource, clientId, clientSecret);
+            SdsService sdsService = new(new Uri(resource), authenticationHandler);
+        }
+    }
+}
+
+
